7fb95906bed0f2509e4e082ca51f2fae3797d1e0
aeg <andreas.edling@tobii.com>
2019-11-30 07:03:47 +0100

    parsing collada wip

--- src/scene/loaders/mod.rs
+++ src/scene/loaders/mod.rs
@@ -1,8 +1,9 @@
 pub mod boxloader;
+pub mod colladaloader;
 
 use super::Scene;
 
 pub trait SceneLoader {
-    fn from_str(s: &str) -> Result<Scene, &str>;
+    fn from_str(input: &str) -> Result<Scene, &str>;
     fn load() -> Result<Scene, &'static str>;
 }
